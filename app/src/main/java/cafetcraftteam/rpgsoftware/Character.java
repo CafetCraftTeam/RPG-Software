8d9e27d3d4f863f5ff91312e63f322541ac6b2b7
Christoph Schaefer <christophernstrerne.schaefer@epfl.ch>
2016-02-10 20:49:02 +0100

    Petit push de début avant de commencer la branche Character. Creé class Character.java et CharacterWarhammer.java héritant de celle-ci. Pour l'instant que des attributs.

---
+++ app/src/main/java/cafetcraftteam/rpgsoftware/Character.java
@@ -0,0 +1,16 @@
+package cafetcraftteam.rpgsoftware;
+
+/**
+ * Created by Pujima on 10/02/2016.
+ */
+public class Character {
+    /*
+    Character and Player Information
+     */
+    private static String mName;
+    private static String mRealName;
+    private static String mCampaign;
+    private static String mYear;
+    private static String mRace;
+
+}
---
+++ app/src/main/java/cafetcraftteam/rpgsoftware/CharacterWarhammer.java
@@ -0,0 +1,54 @@
+package cafetcraftteam.rpgsoftware;
+
+import java.util.Map;
+
+/**
+ * Created by Pujima on 10/02/2016.
+ */
+public class CharacterWarhammer extends Character {
+
+    /*
+    Personal Details
+     */
+    private static int mAge ;
+    private static String mSex;
+    private static String mEyecolor;
+    private static int mHeight ;
+    private static String mHaircolor;
+    private static int mWeight ;
+    private static String mSA ;
+    private static int mNbBrSis ;
+    private static String mBirthPlace;
+    private static String mDistinctifSign;
+        /*
+    Main Profile
+     */
+    
+    private String mProfession;
+    private Map mPrimaryProfile;
+    private Map mSecondaryProfile;
+
+    private int mMovement;
+    private int mTotalFortune;
+    private int mActualFortune;
+    private int mTotalWounds;
+    private int mActualWounds;
+
+            /*
+    Equipment
+     */
+    private Map mWeapons;
+    private Map mArmor;
+    private Map mDefensePoints;
+    private Map mEquipment;
+
+            /*
+    Abilities and Talents
+     */
+
+    private Map mBasicAbilities;
+    private Map mAdvanAbilities;
+
+
+
+}
