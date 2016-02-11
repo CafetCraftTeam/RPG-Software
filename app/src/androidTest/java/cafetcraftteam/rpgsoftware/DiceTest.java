004562743dda1f360c55041cf4cd4e05f58d9fe8
MANQUAT Gautier <gautier.manquat@epfl.ch>
2016-02-11 22:44:52 +0100

    modification des test pour tenir compte de la borne inférieur des dés et ainsi utiliser toute les fonctions de Dice.

--- app/src/androidTest/java/cafetcraftteam/rpgsoftware/DiceTest.java
+++ app/src/androidTest/java/cafetcraftteam/rpgsoftware/DiceTest.java
@@ -3,41 +3,50 @@ package cafetcraftteam.rpgsoftware;
 /**
  * Created by defig on 10.02.2016.
  */
+
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.w3c.dom.Comment;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class DiceTest {
+public class DiceTest
+{
     private Dice D6 = new Dice(6);
-    private Dice D100 = new Dice(100);
+    private Dice D100 = new Dice(2, 100);
 
     @Before
-    public void init() {
+    public void init()
+    {
     }
 
     @Test
-    public void DiceBoundaryTest() {
-        assertEquals(6,D6.getMax());
-        assertEquals(100,D100.getMax());
+    public void DiceBoundaryTest()
+    {
+        assertEquals(6, D6.getMax());
+        assertEquals(1, D6.getInf());
+        assertEquals(100, D100.getMax());
+        assertEquals(2, D100.getInf());
     }
+
     @Test
-    public void DiceOverflowTest() {
-        for (int i=0; i<10000; i++){
+    public void DiceOverflowTest()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
             int number = D6.diceGenerator();
-            assertTrue(number<=6 && number>=1);
+            assertTrue(number <= 6 && number >= 1);
         }
-        for (int i=0; i<10000; i++){
+        for (int i = 0; i < 10000; i++)
+        {
             int number = D100.diceGenerator();
-            assertTrue(number<=100 && number>=1);
+            assertTrue(number <= 100 && number >= 2);
         }
     }
 }
