a3781b103e7883b3d3b6194ad7d2131655fe3efe
MANQUAT Gautier <gautier.manquat@epfl.ch>
2016-02-11 01:07:47 +0100

    Création de la classe Equipment et ajout des attributs et des getter

--- app/src/main/java/cafetcraftteam/rpgsoftware/Equipment.java
+++ app/src/main/java/cafetcraftteam/rpgsoftware/Equipment.java
@@ -2,9 +2,42 @@ package cafetcraftteam.rpgsoftware;
 
 /**
  * Created by Gautier on 10/02/2016.
- *
  */
 public class Equipment
 {
-    
+    public enum Quality
+    {
+        BAD,
+        NORMAL,
+        EXCEPTIONAL
+    }
+
+    private String mName;
+    private int mEncomberment;
+    private Quality mQuatilty;
+    private String mDescription;
+
+    public Equipment(String name, int encomberment, Quality quality, String description)
+    {
+        mName = name;
+        mEncomberment = encomberment;
+        mQuatilty = quality;
+        mDescription = description;
+    }
+
+    public String getName(){
+        return mName;
+    }
+
+    public int getEncomberment() {
+        return mEncomberment;
+    }
+
+    public Quality getQuatilty() {
+        return mQuatilty;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
 }
