454002b159631a5c56ca96d2a39b13ae7f2fc282
MANQUAT Gautier <gautier.manquat@epfl.ch>
2016-02-11 22:31:21 +0100

    nettoyage de la classe de test

--- app/src/androidTest/java/cafetcraftteam/rpgsoftware/CharacterTest.java
+++ app/src/androidTest/java/cafetcraftteam/rpgsoftware/CharacterTest.java
@@ -6,43 +6,20 @@ import android.test.suitebuilder.annotation.LargeTest;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.w3c.dom.Comment;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 
 /**
  * Created by Pujima on 11/02/2016.
  */
-
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class CharacterTest {
 
-    @RunWith(AndroidJUnit4.class)
-    @LargeTest
-    public class DiceTest {
-        private Dice D6 = new Dice(6);
-        private Dice D100 = new Dice(100);
-
-        @Before
-        public void init() {
-        }
-
-        @Test
-        public void DiceBoundaryTest() {
-            assertEquals(6,D6.getMax());
-            assertEquals(100,D100.getMax());
-        }
-        @Test
-        public void DiceOverflowTest() {
-            for (int i=0; i<10000; i++){
-                int number = D6.diceGenerator();
-                assertTrue(number<=6 && number>=1);
-            }
-            for (int i=0; i<10000; i++){
-                int number = D100.diceGenerator();
-                assertTrue(number<=100 && number>=1);
-            }
-        }
+    @Before
+    public void init() {
     }
 
 }
