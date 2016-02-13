3e14e9f5bf9fa8cb9ef967f4afbde4c2107c75c6
MANQUAT Gautier <gothlego@hotmail.com>
2016-02-13 23:48:41 +0100

    reretest

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,10 +6,12 @@ export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
-  cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
-  echo y | android update sdk -u -a -t build-tools-20.0.0 &&
-  echo y | android update sdk -u -a -t android-19 &&
-  echo y | android update sdk -u -a -t extra-android-m2repository &&
-  echo y | android update sdk -u -a -t extra-google-m2repository &&
+  echo y | android update sdk -u -a -t android-23 &&
+  echo y | android update sdk -u -a -t platform-tools &&
+  echo y | android update sdk -u -a -t extra &&
+  echo y | android update sdk -u -a -t build-tools-23.1.0 &&
+  echo y | android update sdk -u -a -t sys-img-x86-android-23 &&
+  echo y | android update sdk -u -a -t addon-google_apis-google-23 &&
+  echo n | android create avd -n testing -f -t android-23 &&
   touch $DEPS
 fi
