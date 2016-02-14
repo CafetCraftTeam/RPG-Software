acaeda086ff3289c8087f2d03bede396c1cf8afb
Christoph Schaefer <christophernstrerne.schaefer@epfl.ch>
2016-02-14 19:06:56 +0100

    Starting Creation of Maincharacter activity and three corresponding Fragments

---
+++ app/src/main/java/cafetcraftteam/rpgsoftware/CharacterActivity.java
@@ -0,0 +1,15 @@
+package cafetcraftteam.rpgsoftware;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+public class CharacterActivity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character);
+    }
+
+}
