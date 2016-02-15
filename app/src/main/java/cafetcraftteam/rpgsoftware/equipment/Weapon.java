bad8d9a0166d47c752b8c17147b645622ab377a2
MANQUAT Gautier <gothlego@hotmail.com>
2016-02-15 01:56:53 +0100

    Ajout des méthodes equals et hashCode à Weapon

--- app/src/main/java/cafetcraftteam/rpgsoftware/equipment/Weapon.java
+++ app/src/main/java/cafetcraftteam/rpgsoftware/equipment/Weapon.java
@@ -1,5 +1,8 @@
 package cafetcraftteam.rpgsoftware.equipment;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 /**
  * Created by Gautier on 11/02/2016.
  */
@@ -24,5 +27,38 @@ public class Weapon extends Equipment
     public String getQualities()
     {
         return mQualities;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+
+        //Verify if it's not the same object
+        if (object == this) {
+            return true;
+        }
+
+        if (!(object instanceof  Weapon)) {
+            return false;
+        }
+
+        Weapon other = (Weapon) object;
+
+        return new EqualsBuilder()
+                .appendSuper(super.equals(object))
+                .append(mGroup, other.mGroup)
+                .append(mQualities, other.mQualities)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(5, 19)
+                .appendSuper(super.hashCode())
+                .append(mGroup)
+                .append(mQualities)
+                .toHashCode();
     }
 }
