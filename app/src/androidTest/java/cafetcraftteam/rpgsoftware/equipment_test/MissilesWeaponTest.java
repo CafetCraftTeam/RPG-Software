136b5bb14afd1407b6f2f2713dd01d1b328bec06
MANQUAT Gautier <gothlego@hotmail.com>
2016-02-18 23:49:57 +0100

    Test du contrat du constructeur de MissilesWeapon

--- app/src/androidTest/java/cafetcraftteam/rpgsoftware/equipment_test/MissilesWeaponTest.java
+++ app/src/androidTest/java/cafetcraftteam/rpgsoftware/equipment_test/MissilesWeaponTest.java
@@ -14,6 +14,7 @@ import cafetcraftteam.rpgsoftware.equipment.MissilesWeapon;
 import cafetcraftteam.rpgsoftware.equipment.Weapon;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.fail;
 
 /**
  * Created by Gautier on 11/02/2016.
@@ -48,5 +49,69 @@ public class MissilesWeaponTest
         assertEquals(mShortRange, mMissilesWeapon.getShortRange());
         assertEquals(mLongRange, mMissilesWeapon.getLongRange());
         assertEquals(mReload, mMissilesWeapon.getReload());
+    }
+
+    @Test
+    public void contractRespectShortRange()
+    {
+        // short range must be positive
+        try
+        {
+            new MissilesWeapon(mName, mEncumbering, mPrice, mQuality, mDescription, mGroup, mQualities,
+                    -mShortRange, mLongRange, mReload);
+            fail("Negative short range don't generate an exception");
+        } catch (IllegalArgumentException e)
+        {
+            // success
+        }
+
+        // short range could be zero
+        MissilesWeapon missilesWeapon = new MissilesWeapon(mName, mEncumbering, mPrice, mQuality,
+                mDescription, mGroup, mQualities, 0, mLongRange, mReload);
+        assertEquals(0, missilesWeapon.getShortRange());
+    }
+
+    @Test
+    public void contractRespectLongRange()
+    {
+        // long range must be positive
+        try
+        {
+            new MissilesWeapon(mName, mEncumbering, mPrice, mQuality, mDescription, mGroup, mQualities,
+                    mShortRange, -mLongRange, mReload);
+            fail("Negative long range don't generate an exception");
+        } catch (IllegalArgumentException e)
+        {
+            // success
+        }
+
+        // long range could be zero
+        MissilesWeapon missilesWeapon = new MissilesWeapon(mName, mEncumbering, mPrice, mQuality,
+                mDescription, mGroup, mQualities, 0, 0, mReload);
+        assertEquals(0, missilesWeapon.getLongRange());
+
+        // long range must be greater than short range or it will be swap
+        MissilesWeapon missilesWeapon1 = new MissilesWeapon(mName, mEncumbering, mPrice, mQuality,
+                mDescription, mGroup, mQualities, mShortRange, 0, mReload);
+        assertEquals(mShortRange, missilesWeapon1.getLongRange());
+    }
+
+    @Test
+    public void contractRespectReload() {
+        // Reload must not be null
+        try
+        {
+            new MissilesWeapon(mName, mEncumbering, mPrice, mQuality, mDescription, mGroup, mQualities,
+                    mShortRange, mLongRange, -mReload);
+            fail("Negative Reload don't generate an exception");
+        } catch (IllegalArgumentException e)
+        {
+            // success
+        }
+
+        // Reload could be zero
+        MissilesWeapon missilesWeapon = new MissilesWeapon(mName, mEncumbering, mPrice, mQuality,
+                mDescription, mGroup, mQualities, mShortRange, mLongRange, 0);
+        assertEquals(0.0, missilesWeapon.getReload());
     }
 }
