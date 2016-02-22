5ed0b2f820ecd31001c182e2159d112673b28da3
Christoph Schaefer <christophernstrerne.schaefer@epfl.ch>
2016-02-22 16:00:37 +0100

    Premier draft de la classe Skill.java On attend la révision de la classe characteristic avant de continuer.

--- app/src/main/java/cafetcraftteam/rpgsoftware/Skill.java
+++ app/src/main/java/cafetcraftteam/rpgsoftware/Skill.java
@@ -7,22 +7,23 @@ public class Skill {
 
     private String mName;
     //Verifier le type enum créé par Christoph dans CharacterWarhammer
-    private Char mChar;
+    private PrimCharacteristic mChar;
     private int mLevel;
     private int mBonus;
 
-    //Verifier la fonction getprofile créé par Christoph dans CharacterWarhammer
+    /*
+    //Attendre révision de la classe characteristic
     public int computeSkillValue(){
         int temp = 10;
         //Verifier la formule de calcul :
-        return getprofile("Ag")*(1+0.1*mLevel) + mBonus;
+        return CharacterWarhammer.getPrimaryProfile(mChar)*(1+0.1*mLevel) + mBonus;
     }
-
+    */
     public String getName() {
         return mName;
     }
     //Verifier le type de retour
-    public Char getChar() {
+    public PrimCharacteristic getChar() {
         return mChar;
     }
 
@@ -43,17 +44,17 @@ public class Skill {
     }
 
     //Changer le type de Char par le enum créé dans Charactere Warhamer
-    public Skill(String Name, Char Charact, int Level, int Bonus) {
+    public Skill(String Name, PrimCharacteristic Charact, int Level, int Bonus) throws Exception {
         //Verifier la longueur max d'un nom
         if(Name.length() > 12 || Name.length() < 1) {
-            throw new Exception("Le nom est trop long ou trop court")
+            throw new Exception("Le nom est trop long ou trop court");
         }
         else if(Level > 2 || Level < 0) {
-            throw new Exception("Le niveau de compétence est trop faible ou trop fort")
+            throw new Exception("Le niveau de compétence est trop faible ou trop fort");
         }
         //Verifier la valeur max du bonus
         else if(Bonus > 5 || Level < 0) {
-            throw new Exception("Le niveau de bonus est trop faible ou trop fort")
+            throw new Exception("Le niveau de bonus est trop faible ou trop fort");
         }
 
         mName = Name;
