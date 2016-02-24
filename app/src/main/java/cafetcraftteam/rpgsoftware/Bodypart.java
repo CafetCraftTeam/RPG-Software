b3e9c319b100ec6236c48c85a3a998a5c73445d7
MANQUAT Gautier <gothlego@hotmail.com>
2016-02-25 00:28:40 +0100

    Correction de BodyPart pour que ça compile

--- app/src/main/java/cafetcraftteam/rpgsoftware/Bodypart.java
+++ app/src/main/java/cafetcraftteam/rpgsoftware/Bodypart.java
@@ -4,5 +4,5 @@ package cafetcraftteam.rpgsoftware;
  * Created by Pujima on 11/02/2016.
  */
 public enum BodyPart {
-    HEAD,TORSO,LEFT_ARM,RIGHT_ARM,LEFT_LEG,RIGHT_LEG
+    HEAD, TORSO, LEFT_ARM, RIGHT_ARM, LEFT_LEG, RIGHT_LEG
 }
