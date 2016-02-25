265bbc6d4624028814372c0f147dfc55820c9ba9
MANQUAT Gautier <gothlego@hotmail.com>
2016-02-25 10:21:35 +0100

    Creation du fichier de test pour Skill

---
+++ app/src/androidTest/java/cafetcraftteam/rpgsoftware/SkillTest.java
@@ -0,0 +1,15 @@
+package cafetcraftteam.rpgsoftware;
+
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.runner.RunWith;
+
+/**
+ * Class that test the skill
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class SkillTest {
+    
+}
