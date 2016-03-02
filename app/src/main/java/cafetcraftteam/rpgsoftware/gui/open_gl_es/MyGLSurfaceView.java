da413eee263adf180b8f79fb5ddcbf65d055e254
MANQUAT Gautier <gothlego@hotmail.com>
2016-03-02 01:54:07 +0100

    Ajout des classes manquantes

---
+++ app/src/main/java/cafetcraftteam/rpgsoftware/gui/open_gl_es/MyGLSurfaceView.java
@@ -0,0 +1,25 @@
+package cafetcraftteam.rpgsoftware.gui.open_gl_es;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+/**
+ * A view container where OpenGL ES graphics can be drawn on screen.
+ * This view can also be used to capture touch events, such as a user
+ * interacting with drawn objects.
+ */
+public class MyGLSurfaceView extends GLSurfaceView {
+    private final MyGLRenderer mRenderer;
+
+    public MyGLSurfaceView(Context openGLActivity) {
+        super(openGLActivity);
+
+        // creating the OpenGL ES 2.0 context
+        super.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+        setEGLContextClientVersion(2);
+
+        // Set the Renderer for drawing on the GLSurfaceView
+        mRenderer = new MyGLRenderer();
+        setRenderer(mRenderer);
+    }
+}
