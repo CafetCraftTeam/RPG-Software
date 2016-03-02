dfdf205ccf8ebf23642d762ee371fcde34e161f9
MANQUAT Gautier <gothlego@hotmail.com>
2016-03-02 02:25:42 +0100

    Ajout de la classe vide Triangle et ajout des fonctions use et unUse dans Program

--- app/src/main/java/cafetcraftteam/rpgsoftware/gui/open_gl_es/Program.java
+++ app/src/main/java/cafetcraftteam/rpgsoftware/gui/open_gl_es/Program.java
@@ -2,18 +2,22 @@ package cafetcraftteam.rpgsoftware.gui.open_gl_es;
 
 import android.opengl.GLES20;
 
+import java.util.Stack;
+
 /**
  * Created by Tago on 02/03/2016.
  */
 public class Program {
+    private static final Stack<Integer> mActualProgramInUse = new Stack<>();
+
     private final Shader mVertexShader;
     private final Shader mFragmentShader;
 
     private final int mId;
 
-    public Program(String vertexShaderCode, String fragementShaderCode) {
+    public Program(String vertexShaderCode, String fragmentShaderCode) {
         mVertexShader = new Shader(Shader.ShaderType.VERTEX, vertexShaderCode);
-        mFragmentShader = new Shader(Shader.ShaderType.FRAGMENT, fragementShaderCode);
+        mFragmentShader = new Shader(Shader.ShaderType.FRAGMENT, fragmentShaderCode);
 
         // create an empty program (code to execute on the GPU)
         mId = GLES20.glCreateProgram();
@@ -23,5 +27,20 @@ public class Program {
         mFragmentShader.attachTo(mId);
         // create the executable of the program
         GLES20.glLinkProgram(mId);
+    }
+
+    public void use() {
+        mActualProgramInUse.add(mId);
+        GLES20.glUseProgram(mId);
+    }
+
+    public void unUse() {
+        mActualProgramInUse.pop();
+        if (mActualProgramInUse.size() > 0)
+        {
+            GLES20.glUseProgram(mActualProgramInUse.peek());
+        } else {
+            GLES20.glUseProgram(0);
+        }
     }
 }
---
+++ app/src/main/java/cafetcraftteam/rpgsoftware/gui/open_gl_es/Triangle.java
@@ -0,0 +1,7 @@
+package cafetcraftteam.rpgsoftware.gui.open_gl_es;
+
+/**
+ * Created by Tago on 02/03/2016.
+ */
+public class Triangle {
+}
