e3ec277b8daeefdc52fde97dad2411f163c5ff0d
MANQUAT Gautier <gothlego@hotmail.com>
2016-03-03 14:31:53 +0100

    Utilisation des Node pour tracer un carré

---
+++ app/src/main/java/cafetcraftteam/rpgsoftware/gui/open_gl_es/NodeBuilder.java
@@ -0,0 +1,29 @@
+package cafetcraftteam.rpgsoftware.gui.open_gl_es;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Gautier on 03/03/2016.
+ */
+public class NodeBuilder implements DrawableBuilder
+{
+    private List<DrawableBuilder> mDrawableBuilders;
+
+    public NodeBuilder(List<DrawableBuilder> drawableBuilders)
+    {
+        mDrawableBuilders = drawableBuilders;
+    }
+
+    @Override
+    public Drawable build()
+    {
+        List<Drawable> drawables = new ArrayList<>();
+
+        for (DrawableBuilder drawableBuilder : mDrawableBuilders)
+        {
+            drawables.add(drawableBuilder.build());
+        }
+        return new Node(drawables);
+    }
+}
--- app/src/main/java/cafetcraftteam/rpgsoftware/gui/open_gl_es/OpenGLActivity.java
+++ app/src/main/java/cafetcraftteam/rpgsoftware/gui/open_gl_es/OpenGLActivity.java
@@ -4,23 +4,42 @@ import android.app.Activity;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Tago on 02/03/2016.
  */
-public class OpenGLActivity extends Activity {
+public class OpenGLActivity extends Activity
+{
     private GLSurfaceView mGLView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
+        // create the node that will be displayed
+        List<DrawableBuilder> drawablesBuilder = new ArrayList<>();
+
+        // in counterclockwise order:
+        float[] vertex1 = {-0.5f, 0.5f, 0.5f};
+        float[] vertex2 = {-0.5f, -0.5f, 0.5f};
+        float[] vertex3 = {0.5f, -0.5f, 0.5f};
+        float[] vertex4 = {0.5f, 0.5f, 0.5f};
+        drawablesBuilder.add(new BasicTriangleBuilder(vertex1, vertex2, vertex3));
+        drawablesBuilder.add(new BasicTriangleBuilder(vertex1, vertex3, vertex4));
+
+        NodeBuilder masterNodeBuilder = new NodeBuilder(drawablesBuilder);
+
         // Create a GLSurfaceView instance and set it as the ContentView for this Activity
-        mGLView = new MyGLSurfaceView(this, new MyGLRenderer(new BasicTriangleBuilder()));
+        mGLView = new MyGLSurfaceView(this, new MyGLRenderer(masterNodeBuilder));
         setContentView(mGLView);
     }
 
     @Override
-    protected void onPause() {
+    protected void onPause()
+    {
         super.onPause();
         // The following call pauses the rendering thread.
         // If your OpenGL application is memory intensive,
@@ -30,7 +49,8 @@ public class OpenGLActivity extends Activity {
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
         // The following call resumes a paused rendering thread.
         // If you de-allocated graphic objects for onPause()
