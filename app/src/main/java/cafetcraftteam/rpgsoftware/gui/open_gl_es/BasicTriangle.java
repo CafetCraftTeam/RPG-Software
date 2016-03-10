1c1d9668da261088047190ef3aafa9224440f50b
MANQUAT Gautier <gothlego@hotmail.com>
2016-03-10 14:20:35 +0100

    Ajout des couleurs dans BasicTriangle

--- app/src/main/java/cafetcraftteam/rpgsoftware/gui/open_gl_es/BasicTriangle.java
+++ app/src/main/java/cafetcraftteam/rpgsoftware/gui/open_gl_es/BasicTriangle.java
@@ -7,11 +7,14 @@ import android.opengl.GLES20;
  * Created by Gautier on 03/03/2016.
  */
 public class BasicTriangle extends Triangle {
+    private static final float[] COLOR = {0.63671875f, 0.76953125f, 0.22265625f, 1.0f};
 
     private BasicProgram mProgram;
 
     public BasicTriangle(float[] vertex1, float[] vertex2, float[] vertex3, Context context) {
-        super(vertex1, vertex2, vertex3, new BasicProgramBuilder(context).build());
+        super(vertex1, vertex2, vertex3,
+                COLOR, COLOR, COLOR,
+                new BasicProgramBuilder(context).build());
         mProgram = (BasicProgram) super.mProgram;
     }
 
@@ -22,7 +25,8 @@ public class BasicTriangle extends Triangle {
                 VERTEX_STRIDE, mVertexBuffer);
 
         // Set color for drawing the triangle
-        mProgram.setColor(1, mColor, 0);
+        mProgram.setColor(SIZE_OF_THE_COLORS, GLES20.GL_FLOAT, false,
+                COLOR_STRIDE, mVertexBuffer);
 
         // MyGLRenderer.checkGlError("glGetUniformLocation");
 
