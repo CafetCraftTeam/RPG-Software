c9a3c4f32e80fb166b56f46998577171f5652d9a
MANQUAT Gautier <gothlego@hotmail.com>
2016-03-14 10:07:16 +0100

    Ajout d'une fonction add a NodeBuilder, permettant d'ajouter des DrawableBuilder arpès la création de l'objet

--- app/src/main/java/cafetcraftteam/rpgsoftware/gui/open_gl_es/NodeBuilder.java
+++ app/src/main/java/cafetcraftteam/rpgsoftware/gui/open_gl_es/NodeBuilder.java
@@ -7,10 +7,14 @@ import java.util.List;
  * Created by Gautier on 03/03/2016.
  */
 public class NodeBuilder implements DrawableBuilder {
-    private List<DrawableBuilder> mDrawableBuilders;
+    private final List<DrawableBuilder> mDrawableBuilders;
 
     public NodeBuilder(List<DrawableBuilder> drawableBuilders) {
         mDrawableBuilders = drawableBuilders;
+    }
+
+    public void add(DrawableBuilder drawableBuilder) {
+        mDrawableBuilders.add(drawableBuilder);
     }
 
     @Override
